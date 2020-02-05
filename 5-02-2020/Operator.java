09d85f87c65e34ddd2e0f21f4ac69c841433faae
Vishwanath  R <52597795+Vishwanath-R@users.noreply.github.com>
2020-02-05 14:51:46 +0530

    Add files via upload

---
+++ 5-02-2020/Operator.java
@@ -0,0 +1,16 @@
+class Operator
+{ 
+  public static void main(String args[])
+  {
+    int a=10;
+    int b=5;
+    System.out.println(a+"+"+b+"="+ (a+b));
+    System.out.println(a+"-"+b+"="+ (a-b));
+    System.out.println(a+"*"+b+"="+ (a*b));
+    System.out.println(a+"/"+b+"="+ (a/b));
+    System.out.println(10<<2); //10*(2^2)=40
+    System.out.println(10<<3);
+    System.out.println(5<<2); //5*(2^2)=20
+    System.out.println(20<<3);
+    System.out.println(15<<4);
+}}
